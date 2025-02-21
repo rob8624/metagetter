76fc477cf750e3c32cd1846aa07689de71156eea
Rob8624 <robert_melen@yahoo.co.uk>
2025-02-21 22:20:34 +0000

    added whitenoise and collectstatic

--- metagetter/settings.py
+++ metagetter/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -145,4 +147,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS=['metagetter-production.up.railway.app', 'localhost']
 
-CSRF_TRUSTED_ORIGINS=['http://localhost:8000']
+CSRF_TRUSTED_ORIGINS=['http://localhost:8000', 'https://metagetter-production.up.railway.app']
