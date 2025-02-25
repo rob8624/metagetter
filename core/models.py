6c47bb273f054c3fb2d7c0c8bc3b235d954a2b21
Rob8624 <robert_melen@yahoo.co.uk>
2025-02-25 12:34:56 +0000

    added pyexif added imgage model, added image to admin

--- core/models.py
+++ core/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
+
+    def get_image_path(self):
+        return self.image.path
+    
+    def __str__(self):
+        return "Image"
