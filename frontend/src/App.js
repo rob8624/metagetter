02f9719cbff6a0b562a40b3aa1c0ca9eb8dc31d9
Rob8624 <robert_melen@yahoo.co.uk>
2025-02-28 19:54:08 +0000

    trying to add utils.js to repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Button } from './components/ui/button'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>srpp.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactasdasdasd
-        </a>
+    
         
         <Button className="bg-blue-100 text-white">Click Me</Button>
-      </header>
-    </div>
-  );
+      
+  )
 }
 
 export default App;
