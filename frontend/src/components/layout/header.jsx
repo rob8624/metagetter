142e9680f6018e29498f942c4501c470d30421b3
Rob8624 <robert_melen@yahoo.co.uk>
2025-03-04 11:17:15 +0000

    added nav component

--- frontend/src/components/layout/header.jsx
+++ frontend/src/components/layout/header.jsx
@@ -2,19 +2,37 @@ import React from "react";
 import {
     NavigationMenu,
     NavigationMenuContent,
-    NavigationMenuIndicator,
+    
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
     NavigationMenuTrigger,
-    NavigationMenuViewport,
+   
   } from "../ui/navigation-menu"
 
 const Header = () => {
   
   return (
   <div className='flex flex-row justify-center'> 
-     <h1>MetaGetter</h1>
+    <div className="flex flex-col">
+        <h1>MetaGetter</h1>
+            <div>
+                <NavigationMenu >
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                        <NavigationMenuTrigger className="bg-slate-300">About</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <div className="p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                                    <h1>Welcome to MetaGetter</h1>
+                                <NavigationMenuLink>About</NavigationMenuLink>
+                                <NavigationMenuLink>Usage</NavigationMenuLink>
+                                </div>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+     </div>
   </div>
  )
 }
