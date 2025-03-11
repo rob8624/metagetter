cfb2af7796cba10074cdab6dd5ed6c8deba796c3
Rob8624 <robert_melen@yahoo.co.uk>
2025-03-11 01:10:02 +0000

    fixed typo on burger path

--- frontend/src/components/custom/mobileMenu.jsx
+++ frontend/src/components/custom/mobileMenu.jsx
@@ -12,7 +12,7 @@ export default function MobileMenu () {
         return(
   <Drawer>
     <DrawerTrigger>
-    <img className="h-10 m-2 rounded-md opacity-50 "src="/Burger-menu.svg" alt="Logo" />
+    <img className="h-10 m-2 rounded-md opacity-50" src="/burger-menu.svg" alt="Logo" />
     </DrawerTrigger>
     <DrawerContent >
       <div className="flex  justify-center pb-10">
