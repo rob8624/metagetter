867b790f81fa28acab056fa0571d0f0ea54f503d
Rob8624 <robert_melen@yahoo.co.uk>
2025-03-15 01:05:28 +0000

    added login box diplsay working OK

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -1,9 +1,23 @@
+import { LoginForm } from "../components/ui/login-form"
+import { Link } from 'react-router-dom';
+import { Button } from "../components/ui/button"
+
+
 
 
 export default function SignIn() {
     return (
-        <div className="w-full h-screen flex justify-center items-center bg-blue-100">
-            <h1 className="text-3xl font-bold text-blue-600">SignIn Page</h1>
+        <>
+        <div className="grid grid-cols-1 place-items-center gap-5">
+            <Button>
+                <Link to="/" className="">BACK</Link>
+            </Button>
+            <div className="shadow-2xl">
+                <LoginForm>
+                    dasd
+                </LoginForm>
+            </div>
         </div>
+        </>
     );
 }
