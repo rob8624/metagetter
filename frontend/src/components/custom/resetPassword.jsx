4b8162b04fd63deff80e1b9eabfcd348d00d1826
Rob8624 <robert_melen@yahoo.co.uk>
2025-04-10 18:57:40 +0100

    state fixed on reset dialog

--- frontend/src/components/custom/resetPassword.jsx
+++ frontend/src/components/custom/resetPassword.jsx
@@ -15,9 +15,10 @@ import {
   import { Input } from "../../components/ui/input"
 
   export default function ResetPassword() {
-
+    
     const [email, setEmail] = useState('')
     const [reset, setReset] = useState(false)
+    const [complete, setComplete] = useState(false)
 
     
 
@@ -27,22 +28,39 @@ import {
 
     const recaptchaKey = process.env.REACT_APP_RECAPTCHA_KEY
 
-    function handleReset(e) {
+    async function handleReset(e) {
       e.stopPropagation();
         e.preventDefault()
         
-        console.log('hello')
-        authService.reset(email)
+       try {
+        await authService.reset(email)
+        setReset(false)
+        setComplete(true)
+      }
+       catch (error) {
+        console.error('reset failed', error)
+       }
       }
 
     function handleChange() {
       setReset(true)
     }
-     
+
+    function handleClose() {
+      setComplete(false)
+      setReset(false)
+    }
+
+    
+
+    
+     return (
   
-  return (
-  
-        <Dialog modal={false}>
+        <Dialog modal={false} onOpenChange={(isOpen) => {
+          if (!isOpen) {
+            handleClose(); 
+          }
+        }}>
         <DialogTrigger className="text-xs">Password Reset</DialogTrigger>
         <DialogContent onInteractOutside={handleOutsideClick}>
             <DialogHeader>
@@ -51,14 +69,13 @@ import {
                 <div className="flex flex-col gap-2">
                   <form onSubmit={handleReset} >
                     <span>Enter your registered email below reset password.</span>
-                    
-                  
-                      <Input type="email" name="email"
+                    <Input type="email" name="email"
                       placeholder="Enter your email" 
                       value={email} 
                       onChange={(e) => setEmail(e.target.value) }
                       required />
                       {reset && <Button type="submit" className="mt-2">Reset</Button>}
+                      {complete && <div>Password link has now been sent check inbox</div>}
                     </form>
                 </div>
                 <ReCAPTCHA className="pt-10"
