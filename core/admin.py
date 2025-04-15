3965134e62838b110ff29aa594b1e19df25af58e
Rob8624 <robert_melen@yahoo.co.uk>
2025-04-15 23:56:34 +0100

    added profile as admin inline

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image, Profile
+from django.contrib.auth.models import User  # Import User from auth models
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+admin.site.unregister(User)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,) 
+
+
+admin.site.register(User, UserAdmin)  # First arg is the model, second is the admin class
+
 
 admin.site.register(Image)
